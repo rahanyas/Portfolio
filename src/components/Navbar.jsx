c9303f8055ead703fdb843ea17ebe9ab311d0e7e
RAHANYAS <153750452+rahanyas@users.noreply.github.com>
2025-04-08 21:20:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+ import { useState, useRef, useEffect } from 'react';
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
